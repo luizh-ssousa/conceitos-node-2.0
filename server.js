2d0abad609b201d1a8151ca8664488a12c0b5afc
luizh-ssousa <luizh.ssousa@gmail.com>
2025-01-22 22:08:43 -0300

    resposta da rota

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express"
 
 const app = express()
 
-app.get('/usuarios', (req, res))
+app.get('/usuarios', (req, res) => {
+    res.send("Olá, rota acessada com sucesso!")
+})
 
 app.listen(3000)
